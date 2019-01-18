344a5c7ae567388ee1612a73ce96a1927e9225c0
Viacheslav Dushin <slava@dushin.pro>
2019-01-18 14:34:23 +0300

    moving f77 to a separet dir

--- mk_files/common.mk
+++ mk_files/common.mk
@@ -5,3 +5,6 @@ all: $(TARGET)
 
 %.o: $(SRC_DIR)/%.f90 $(MODS) $(MOD_OBJ) $(MAKES) Makefile
 	$(FF) -c $(CFLAGS) $<
+
+%.o: $(LIB_DIR)/%.f $(MAKES) Makefile
+	$(FF) -c $(CFLAGS) $<
--- mk_files/std_defs.mk
+++ mk_files/std_defs.mk
@@ -1,5 +1,6 @@
 TARGET = cycl
 SRC_DIR = $(DIR_PREFIX)/src
+LIB_DIR = $(DIR_PREFIX)/lib
 MODS_DIR = $(DIR_PREFIX)/mods
 MAKES_DIR = $(DIR_PREFIX)/mk_files
 BUILD_DIR = $(DIR_PREFIX)/build
@@ -10,6 +11,7 @@ FC = $(FF)
 CFLAGS = -O0 -w -I ../mods
 
 SRC_OBJ = $(patsubst $(SRC_DIR)/%.f90,%.o,$(wildcard $(SRC_DIR)/*.f90))
+LIB_OBJ = $(patsubst $(LIB_DIR)/%.f,%.o,$(wildcard $(LIB_DIR)/*.f))
 MAKES = $(wildcard $(DIR_PREFIX)/mk_files/*.mk)
 MODS = $(patsubst $(MODS_DIR)/%.f90,%.mod,$(wildcard $(MODS_DIR)/*.f90))
 MOD_OBJ = $(patsubst $(MODS_DIR)/%.f90,%.o,$(wildcard $(MODS_DIR)/*.f90))
