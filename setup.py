2303763e8e9d30493f918bb2a947219b876db805
Viacheslav Dushin <slava@dushin.pro>
2019-01-25 16:30:08 +0300

    adding setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py-greens-functions',
+    version='0.0.1',
+    description="Python Green's functions implementation",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/slavad/py-series-clean',
+    author='Alexander Mushtukov,Viacheslav Dushin',
+    keywords="Green's functions",
+    packages=find_packages(),
+    classifiers=[
+        #TODO: test with other versions
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=[
+       'numpy>=1.15.0<2.0.0'
+    ]
+)
