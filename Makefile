513fc448d5fa7ef5888d4417c9b050776ba5b596
Viacheslav Dushin <slava@dushin.pro>
2019-01-30 14:23:56 +0300

    fix in build procedure

--- Makefile
+++ Makefile
@@ -1,24 +1,10 @@
-DIR_PREFIX = .
-
-include mk_files/std_defs.mk
-
-include mk_files/common.mk
-
-ALL_OBJS_FOR_WRAPPER=$(addprefix $(BUILD_DIR)/,$(SRC_OBJ_FOR_PY) $(LIB_OBJF90) $(LIB_OBJF77))
-
-$(TARGET): Makefile
-	cd $(BUILD_DIR) && make
-
-wrapper_objs: Makefile
-	cd $(BUILD_DIR) && make wrapper_objs
-
-wrapper: wrapper_objs Makefile
-	python $(SETUP_PY) build_ext --inplace --link-objects="$(ALL_OBJS_FOR_WRAPPER)"
+wrapper: Makefile
+	python setup.py build_ext --inplace
 
 .PHONY: clean
 
 clean:
-	cd $(BUILD_DIR) && make clean
+	cd build && make clean
 	rm -f *.so
 	rm -rf *.dSYM
 
--- setup.py
+++ setup.py
@@ -1,43 +1,45 @@
 # Always prefer setuptools over distutils
+from __future__ import division, absolute_import, print_function
 from setuptools import setup, find_packages
 from os import path
+from numpy.distutils.core import Extension
+from numpy.distutils.core import setup
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(
-    name='py-greens-functions',
-    version='0.0.1',
-    description="Python Green's functions implementation",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/slavad/py-series-clean',
-    author='Alexander Mushtukov, Viacheslav Dushin',
-    keywords="Green's functions",
-    packages=find_packages(),
-    classifiers=[
-        #TODO: test with other versions
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Fortran :: 77',
-        'Programming Language :: Fortran :: 90',
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License'
-    ],
-    install_requires=[
-       'numpy>=1.15.0<2.0.0'
-    ]
-)
-
-from numpy.distutils.core import Extension
-
 ext1 = Extension(name = 'greens',
     sources = [
-        'src/GreenFunLip.f90'
+        'src/GreenFunLip.f90',
+        'src/greens.pyf',
+        'src/mf_BesselMod1.f90',
+        'src/mf_c.f90',
+        'libf90/lib_specfun.f90',
+        'libf77/mf_DIN_djmuz.f'
     ]
 )
-
-from numpy.distutils.core import setup
-setup(name = 'py-greens-functions.flib',
-    ext_modules = [ext1]
-)
+if __name__ == "__main__":
+    setup(
+        name='py-greens-functions',
+        version='0.0.1',
+        description="Python Green's functions implementation",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/slavad/py-series-clean',
+        author='Alexander Mushtukov, Viacheslav Dushin',
+        keywords="Green's functions",
+        packages=find_packages(),
+        classifiers=[
+            #TODO: test with other versions
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Fortran :: 77',
+            'Programming Language :: Fortran :: 90',
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License'
+        ],
+        install_requires=[
+           'numpy>=1.15.0<2.0.0'
+        ],
+        ext_modules = [ext1]
+    )
